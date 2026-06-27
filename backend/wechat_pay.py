03734ed642a0f163db31d0abf434f26e4717a480
bruce <2955678348@qq.com>
2026-06-27 19:30:41 +0800

    神奇九转市值限制

--- backend/wechat_pay.py
+++ backend/wechat_pay.py
@@ -135,7 +135,8 @@ def create_prepay_order(order_no: str, amount_cents: int, description: str, open
             timeout=10
         )
         logger.info(f"微信下单resp: {resp.text}")
-
+        logger.info(f"微信下单resp-code: {resp.status_code}")
+        logger.info(f"微信下单resp: {resp}")
         if resp.status_code == 200:
             data = resp.json()
             return {"prepay_id": data.get("prepay_id")}
